c0040a1d7a9961fbae1d55280279440b4cab757d
1cupcelery <126630780+1cupcelery@users.noreply.github.com>
2023-06-25 18:14:05 +0800

    交一下看看

---
+++ raytracer/src/ray.rs
@@ -0,0 +1,34 @@
+use crate::vec3::Vec3;
+use crate::vec3::Point3;
+
+#[derive(Clone, Debug, PartialEq)]
+
+pub struct Ray {
+    pub orig : Point3,
+    pub dir : Vec3
+}
+
+impl Ray {
+    pub fn new(origin: &Point3, direction : &Vec3) -> Self {
+        Self {
+            orig: Point3{
+                x:origin.x,
+                y:origin.y,
+                z:origin.z,
+            },
+            dir: Vec3 {
+                x:direction.x,
+                y:direction.y,
+                z:direction.z,
+            }
+        }
+    }
+
+    pub fn at(&self,t:f64)->Point3{
+        Point3{
+            x: self.orig.x+t* self.dir.x,
+            y: self.orig.y+t* self.dir.y,
+            z: self.orig.z+t* self.dir.z,
+        }
+    }
+}
--- raytracer/src/vec3.rs
+++ raytracer/src/vec3.rs
@@ -1,22 +1,26 @@
-use std::ops::{Add, AddAssign};
+use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Sub, SubAssign};
 
-#[derive(Clone, Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq, Copy)]
+
 pub struct Vec3 {
-    x: f64,
-    y: f64,
-    z: f64,
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
 }
 
 impl Vec3 {
     pub fn x(&self) -> f64 {
         self.x
     }
+
     pub fn y(&self) -> f64 {
         self.y
     }
+
     pub fn z(&self) -> f64 {
         self.z
     }
+
     pub fn new(x: f64, y: f64, z: f64) -> Self {
         Self { x, y, z }
     }
@@ -29,14 +33,40 @@ impl Vec3 {
         Self::new(0.0, 0.0, 0.0)
     }
 
-    pub fn squared_length(&self) -> f64 {
+    pub fn length_squared(&self) -> f64 {
         self.x * self.x + self.y * self.y + self.z * self.z
+    }
+
+    pub fn length(&self) -> f64 {
+        self.length_squared().sqrt()
+    }
+
+    pub fn dot(&self,other:Self)->f64{
+        self.x * other.x+
+        self.y * other.y+
+        self.z * other.z
+    }
+
+    pub fn cross(&self,other:Self)->Self{
+        Self {
+            x: self.y * other.z - other.y * self.z,
+            y: self.z * other.x - other.z * self.x,
+            z: self.x * other.y - other.x * self.y,
+        }
+    }
+
+    pub fn unit_vector(&self)-> Self{
+        //if self.squared_length() == 0.0?
+        Self{
+            x: self.x/self.length(),
+            y: self.y/self.length(),
+            z: self.z/self.length(),
+        }
     }
 }
 
 impl Add for Vec3 {
     type Output = Self;
-
     fn add(self, other: Self) -> Self {
         Self {
             x: self.x + other.x,
@@ -44,11 +74,10 @@ impl Add for Vec3 {
             z: self.z + other.z,
         }
     }
-}
+}//+
 
 impl Add<f64> for Vec3 {
     type Output = Self;
-
     fn add(self, other: f64) -> Self {
         Self {
             x: self.x + other,
@@ -66,122 +95,122 @@ impl AddAssign for Vec3 {
             z: self.z + other.z,
         };
     }
+}//+=
+
+impl AddAssign<f64> for Vec3 {
+    fn add_assign(&mut self, other: f64) {
+        *self = Self {
+            x: self.x + other,
+            y: self.y + other,
+            z: self.z + other,
+        };
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn test_new() {
-        assert_eq!(Vec3::new(1.0, 2.0, 3.0), Vec3::new(1.0, 2.0, 3.0));
+impl Sub for Vec3 {
+    type Output = Self;
+    fn sub(self, other: Self) -> Self {
+        Self {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+        }
     }
-    #[test]
-    fn test_add() {
-        assert_eq!(
-            Vec3::new(1.0, 0.0, -1.0) + Vec3::new(2.0, 4.0, 6.0),
-            Vec3::new(3.0, 4.0, 5.0)
-        )
-    }
-    #[test]
-    fn test_add_assign() {
-        let mut x = Vec3::new(1.0, 0.0, -1.0);
-        x += Vec3::new(2.0, 4.0, 6.0);
-        assert_eq!(x, Vec3::new(3.0, 4.0, 5.0))
-    }
-    #[test]
-    fn test_add_f64() {
-        assert_eq!(
-            Vec3::new(1.0, 0.0, -1.0) + 233.0,
-            Vec3::new(234.0, 233.0, 232.0)
-        )
-    }
-    // #[test]
-    // fn test_add_assign_f64() {
-    //     let mut x = Vec3::new(1.0, 0.0, -1.0);
-    //     x += 233.0;
-    //     assert_eq!(x, Vec3::new(234.0, 233.0, 232.0))
-    // }
-    // #[test]
-    // fn test_sub() {
-    //     assert_eq!(
-    //         Vec3::new(1.0, 0.0, -1.0) - Vec3::new(2.0, 4.0, 6.0),
-    //         Vec3::new(-1.0, -4.0, -7.0)
-    //     )
-    // }
-    // #[test]
-    // fn test_sub_assign() {
-    //     let mut x = Vec3::new(1.0, 0.0, -1.0);
-    //     x -= Vec3::new(2.0, 4.0, 6.0);
-    //     assert_eq!(x, Vec3::new(-1.0, -4.0, -7.0))
-    // }
-    // #[test]
-    // fn test_sub_f64() {
-    //     assert_eq!(Vec3::new(1.0, 0.0, -1.0) - 1.0, Vec3::new(0.0, -1.0, -2.0))
-    // }
-    // #[test]
-    // fn test_sub_assign_f64() {
-    //     let mut x = Vec3::new(1.0, 0.0, -1.0);
-    //     x -= 1.0;
-    //     assert_eq!(x, Vec3::new(0.0, -1.0, -2.0))
-    // }
-    // #[test]
-    // fn test_mul() {
-    //     assert_eq!(Vec3::new(1.0, 0.0, -1.0) * Vec3::ones(), 0.0);
-    // }
-    // #[test]
-    // fn test_mul_assign() {
-    //     let mut x = Vec3::new(1.0, 0.0, -1.0);
-    //     x *= 2.0;
-    //     assert_eq!(x, Vec3::new(2.0, 0.0, -2.0));
-    // }
-    // #[test]
-    // fn test_mul_f64() {
-    //     assert_eq!(Vec3::new(1.0, 0.0, -1.0) * 1.0, Vec3::new(1.0, 0.0, -1.0));
-    // }
-    // #[test]
-    // fn test_div() {
-    //     assert_eq!(Vec3::new(1.0, -2.0, 0.0) / 2.0, Vec3::new(0.5, -1.0, 0.0));
-    // }
-    // #[test]
-    // fn test_elemul() {
-    //     assert_eq!(
-    //         Vec3::elemul(Vec3::new(1.0, 2.0, 3.0), Vec3::new(1.0, 2.0, 3.0)),
-    //         Vec3::new(1.0, 4.0, 9.0)
-    //     );
-    // }
-    // #[test]
-    // fn test_cross() {
-    //     assert_eq!(
-    //         Vec3::cross(Vec3::new(1.0, 2.0, 3.0), Vec3::new(2.0, 3.0, 4.0)),
-    //         Vec3::new(8.0 - 9.0, 6.0 - 4.0, 3.0 - 4.0)
-    //     );
-    // }
-    // #[test]
-    // fn test_neg() {
-    //     assert_eq!(-Vec3::new(1.0, -2.0, 3.0), Vec3::new(-1.0, 2.0, -3.0));
-    // }
-    #[test]
-    fn test_squared_length() {
-        assert_eq!(Vec3::new(1.0, 2.0, 3.0).squared_length(), 14.0);
-    }
-    // #[test]
-    // fn test_length() {
-    //     assert_eq!(
-    //         Vec3::new(3.0, 4.0, 5.0).length(),
-    //         ((3.0 * 3.0 + 4.0 * 4.0 + 5.0 * 5.0) as f64).sqrt()
-    //     );
-    // }
-    // #[test]
-    // fn test_unit() {
-    //     assert_eq!(Vec3::new(233.0, 0.0, 0.0).unit(), Vec3::new(1.0, 0.0, 0.0));
-    //     assert_eq!(
-    //         Vec3::new(-233.0, 0.0, 0.0).unit(),
-    //         Vec3::new(-1.0, 0.0, 0.0)
-    //     );
-    // }
-    // #[test]
-    // #[should_panic]
-    // fn test_unit_panic() {
-    //     Vec3::new(0.0, 0.0, 0.0).unit();
-    // }
 }
+
+impl Sub<f64> for Vec3 {
+    type Output = Self;
+    fn sub(self, other: f64) -> Self {
+        Self {
+            x: self.x - other,
+            y: self.y - other,
+            z: self.z - other,
+        }
+    }
+}
+
+impl SubAssign for Vec3 {
+    fn sub_assign(&mut self, other: Self) {
+        *self = Self {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+        };
+    }
+}
+
+impl SubAssign<f64> for Vec3 {
+    fn sub_assign(&mut self, other: f64) {
+        *self = Self {
+            x: self.x - other,
+            y: self.y - other,
+            z: self.z - other,
+        };
+    }
+}
+
+impl Mul for Vec3 {
+    type Output = Self;
+    fn mul(self, other: Self) -> Self {
+        Self {
+            x: self.x * other.x,
+            y: self.y * other.y,
+            z: self.z * other.z,
+        }
+    }
+}
+
+impl Mul<f64> for Vec3 {
+    type Output = Self;
+    fn mul(self, other: f64) -> Self {
+        Self {
+            x: self.x * other,
+            y: self.y * other,
+            z: self.z * other,
+        }
+    }
+}
+
+impl MulAssign for Vec3 {
+    fn mul_assign(&mut self, other: Self) {
+        *self = Self {
+            x: self.x * other.x,
+            y: self.y * other.y,
+            z: self.z * other.z,
+        };
+    }
+}
+
+impl MulAssign<f64> for Vec3 {
+    fn mul_assign(&mut self, other: f64) {
+        *self = Self {
+            x: self.x * other,
+            y: self.y * other,
+            z: self.z * other,
+        };
+    }
+}
+
+impl Div<f64> for Vec3 {
+    type Output = Self;
+    fn div(self, other: f64) -> Self {
+        Self {
+            x: self.x / other,
+            y: self.y / other,
+            z: self.z / other,
+        }
+    }
+}
+
+impl DivAssign<f64> for Vec3 {
+    fn div_assign(&mut self, other: f64) {
+        *self = Self {
+            x: self.x / other,
+            y: self.y / other,
+            z: self.z / other,
+        };
+    }
+}
+
+pub type Color= Vec3;
+pub type Point3= Vec3;
