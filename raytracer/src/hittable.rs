b2d0d35a9cd512774007cd03f85c5972e1d51c27
1cupcelery <126630780+1cupcelery@users.noreply.github.com>
2023-06-25 18:29:54 +0800

    Merge remote-tracking branch 'origin/master'

--- raytracer/src/hittable.rs
+++ raytracer/src/hittable.rs
@@ -1,12 +1,12 @@
-use crate::Vec3;
-use crate::vec3::Point3;
-use crate::ray;
-
-struct hit_record {
-    p: Point3,
-    normal: Vec3,
-    t: f64,
-}
+// use crate::Vec3;
+// use crate::vec3::Point3;
+// use crate::ray;
+//
+// struct hit_record {
+//     p: Point3,
+//     normal: Vec3,
+//     t: f64,
+// }
 // class hittable {
 // public:
 // virtual bool hit(const ray& r, double t_min, double t_max, hit_record& rec) const = 0;
