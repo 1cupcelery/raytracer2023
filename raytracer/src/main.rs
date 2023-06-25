9a4f6600624805a8ca35f32824c7438e425eb5c0
1cupcelery <126630780+1cupcelery@users.noreply.github.com>
2023-06-25 19:27:48 +0800

    Merge remote-tracking branch 'origin/master'

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -125,7 +125,7 @@ fn main() {
         for i in 0..width {
             let u = i as f64 / (width - 1) as f64;
             let v = j as f64 / (height - 1) as f64;
-            let r = Ray{
+            let r = Ray {
                 orig: origin,
                 dir: lower_left_corner + horizontal.mul(u) + vertical.mul(v) - origin,
             };
