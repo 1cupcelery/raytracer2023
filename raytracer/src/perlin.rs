8f8351c0b4d9383d3fac701c90a5d37f6073c39e
1cupcelery <126630780+1cupcelery@users.noreply.github.com>
2023-07-02 14:55:08 +0800

    Image 8: Perlin texture with trilinear interpolation

--- raytracer/src/perlin.rs
+++ raytracer/src/perlin.rs
@@ -25,10 +25,41 @@ impl Perlin {
     }
 
     pub fn noise(&self, p: &Point3) -> f64 {
-        let i = (((4.0 * p.x()) as i32) & 255) as usize;
-        let j = (((4.0 * p.y()) as i32) & 255) as usize;
-        let k = (((4.0 * p.z()) as i32) & 255) as usize;
-        self.ranfloat[self.perm_x[i] ^ self.perm_y[j] ^ self.perm_z[k]]
+        let u = p.x() - p.x().floor();
+        let v = p.y() - p.y().floor();
+        let w = p.z() - p.z().floor();
+        let i = p.x().floor();
+        let j = p.y().floor();
+        let k = p.z().floor();
+        let mut c: Vec<Vec<Vec<f64>>> = vec![
+            vec![vec![0.0, 0.0], vec![0.0, 0.0]],
+            vec![vec![0.0, 0.0], vec![0.0, 0.0]],
+        ];
+        for (di, item1) in c.iter_mut().enumerate().take(2) {
+            for (dj, item2) in item1.iter_mut().enumerate().take(2) {
+                for (dk, item3) in item2.iter_mut().enumerate().take(2) {
+                    *item3 = self.ranfloat[self.perm_x[((i + di as f64) as i32 & 255) as usize]
+                        ^ self.perm_y[((j + dj as f64) as i32 & 255) as usize]
+                        ^ self.perm_z[((k + dk as f64) as i32 & 255) as usize]];
+                }
+            }
+        }
+        Self::trilinear_interp(&c, u, v, w)
+    }
+
+    fn trilinear_interp(c: &[Vec<Vec<f64>>], u: f64, v: f64, w: f64) -> f64 {
+        let mut accum = 0.0;
+        for (i, item1) in c.iter().enumerate().take(2) {
+            for (j, item2) in item1.iter().enumerate().take(2) {
+                for (k, _item3) in item2.iter().enumerate().take(2) {
+                    accum += (i as f64 * u + (1 - i) as f64 * (1.0 - u))
+                        * (j as f64 * v + (1 - j) as f64 * (1.0 - v))
+                        * (k as f64 * w + (1 - k) as f64 * (1.0 - w))
+                        * c[i][j][k];
+                }
+            }
+        }
+        accum
     }
 
     fn perlin_generate_perm() -> Vec<usize> {
